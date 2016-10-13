6d30ba8e63723b2b3b8b4de2b41ae88e24d1ab69
Mitya Teryaew <mitya.teryaew@gmail.com>
2016-10-13 18:05:40 +0300

    Remove babel-plugin-react-require

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 module.exports = function(gemini, options) {
-    if (options.plugins && options.plugins.isArray()) {
-        options.plugins.push('babel-plugin-react-require');
-    } else {
-        options.plugins = ['babel-plugin-react-require'];
-    }
-
     require('babel-register')(options);
     require('ignore-styles');
 };
